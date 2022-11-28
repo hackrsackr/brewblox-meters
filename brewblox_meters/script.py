7b7b8832bb071a2b8c184b05cb18bf54752508fc
hackrsackr <rob.knighton10@gmail.com>
2022-11-28 11:26:17 -0500

    remove_flow_meters

--- brewblox_meters/script.py
+++ brewblox_meters/script.py
@@ -1,10 +1,9 @@
 """
 main script
-reads 4 ads1115 ADC boards outputs into one dictionary
+reads up to 4 ads1115 ADC boards outputs into one dictionary
 publishes output to brewblox over mqtt
 """
 import json
-import serial
 
 from time import sleep
 
@@ -47,13 +46,6 @@ ads2_keys = ['output2-1', 'output2-2', 'output2-3', 'output2-4']
 ads3_keys = ['output3-1', 'output3-2', 'output3-3', 'output3-4']
 ads4_keys = ['output4-1', 'output4-2', 'output4-3', 'output4-4']
 
-# USB port of esp32 thats reading flowmeters
-FLOWMETER_SERIAL_PORT = '/dev/ttyUSB0'
-
-ser = serial.Serial(port=FLOWMETER_SERIAL_PORT,
-                    baudrate=115200,
-                    timeout=1)
-
 
 def main():
     try:
@@ -70,9 +62,9 @@ def main():
                 m1.ads = ads1
 
                 d1[m1.name] = {
-                        'mA': round(m1.read_ma(index), 2),
-                        'pH': round(m1.ma_to_ph(m1.ma), 2),
-                        'ORP': round(m1.ma_to_orp(m1.ma), 2)
+                    'mA': round(m1.read_ma(index), 2),
+                    'pH': round(m1.ma_to_ph(m1.ma), 2),
+                    'ORP': round(m1.ma_to_orp(m1.ma), 2)
                 }
 
             # Iterate through ads2 channels, populate dict d2
@@ -83,9 +75,9 @@ def main():
                 m2.ads = ads2
 
                 d2[m2.name] = {
-                        'mA': round(m2.read_ma(index), 2),
-                        'pH': round(m2.ma_to_ph(m2.ma), 2),
-                        'ORP': round(m2.ma_to_orp(m2.ma), 2)
+                    'mA': round(m2.read_ma(index), 2),
+                    'pH': round(m2.ma_to_ph(m2.ma), 2),
+                    'ORP': round(m2.ma_to_orp(m2.ma), 2)
                 }
 
             # Iterate through ads3 channels, populate dict d3
@@ -97,28 +89,19 @@ def main():
                 v.ads = ads3
 
                 d3[v.name] = {
-                        'adc': v.read_ads(index),
-                        'trimmed-adc': v.trim_adc(v.adc, adc3_offsets[index]),
-                        'volts': round(v.read_volts(index), 2),
-                        'liters': round(v.adc_to_liters(), 2),
-                        'gallons': round(v.adc_to_gallons(), 2)
+                    'adc': v.read_ads(index),
+                    'trimmed-adc': v.trim_adc(v.adc, adc3_offsets[index]),
+                    'volts': round(v.read_volts(index), 2),
+                    'liters': round(v.adc_to_liters(), 2),
+                    'gallons': round(v.adc_to_gallons(), 2)
                 }
-
-            d4 = {}
-            flow_data = ser.readline().decode().rstrip()
-            try:
-                flow_data = json.loads(flow_data)
-            except json.JSONDecodeError:
-                continue
-            d4 = flow_data
 
             # Output
             message = {
                 'key': 'meters',
                 'data': {'meter-1': d1,
                          'meter-2': d2,
-                         'volume-sensors': d3,
-                         'flow-meters': d4}
+                         'volume-sensors': d3}
             }
 
             client.publish(TOPIC, json.dumps(message))
@@ -126,7 +109,6 @@ def main():
             sleep(5)
 
     finally:
-        ser.close()
         client.loop_stop
 
 
